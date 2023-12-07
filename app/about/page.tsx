682e8856bd73e275c7166559db3ffea417b83679
nelsonsaake <=>
2023-12-07 23:26:53 +0000

    firt commit

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function AboutPage(){
+    return (
+        <div className='p-5 flex flex-col space-y-5'>
+            <h2>app/about/page.tsx</h2>
+
+            <button className="btn btn-primary">primary</button>
+            <button className="btn btn-success">success</button>
+        </div>
+    )
+}
